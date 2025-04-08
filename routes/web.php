5bc20e0532e6967278d99837298f9ba5288c20d3
Andre M. Bertachini <andre@bertachini.org>
2025-04-07 21:50:14 -0300

    Esboco de login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+})->name('login');
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+    if (Auth::attempt($credentials)) {
+        return redirect('/home');
+    }
+    return back()->withErrors(['invalid-credentials' => 'Invalid credentials']);
 });
+
+Route::get('/home', function() {
+    return view('home');
+})->middleware('auth');
