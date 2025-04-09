0b7a96fd4714f53e41c3526a68ccad7f61a60194
Andre M. Bertachini <andre@bertachini.org>
2025-04-09 20:51:42 -0300

    Segundo esboco de login

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,20 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\AuthController;
+
 Route::get('/', function () {
     return view('login');
 })->name('login');
 
-Route::post('/login', function (Request $request) {
-    $credentials = $request->only('email', 'password');
-    if (Auth::attempt($credentials)) {
-        return redirect('/home');
-    }
-    return back()->withErrors(['invalid-credentials' => 'Invalid credentials']);
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/login', 'login')->name('login');
+    Route::post('/login', 'tryLogin')->name('try.login');
+
+    Route::get('/register', 'register')->name('register');
+    Route::post('/register', 'saveRegister')->name('save.register');
+
+    Route::get('/logout', 'logout')->name('logout');
 });
 
 Route::get('/home', function() {
