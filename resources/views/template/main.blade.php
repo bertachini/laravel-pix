86b3afba3886c32590cf7cd47eb9ae6ad8234a92
Eduardo Gustavo <66931597+dudugudu@users.noreply.github.com>
2025-04-09 20:59:23 -0300

    init front

---
+++ resources/views/template/main.blade.php
@@ -0,0 +1,23 @@
+
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+
+    </head>
+    <body class="">
+    <nav class="navbar">
+        <div class="logo">
+            <img src="{{asset('img/laravel.svg')}}" alt="Logo">
+            <Label>Laravel Pix </Label>
+        </div>
+        <div class="user-icon" title="Usuário Logado"></div>
+    </nav>
+        
+    </body>
+</html>
