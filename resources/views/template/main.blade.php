ec486ade1f3ae76904ba2b0aa06d8dbd5e16fbfa
Eduardo Gustavo <66931597+dudugudu@users.noreply.github.com>
2025-04-11 20:22:26 -0300

    update

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -1,23 +1,25 @@
-
 <html lang="pt-BR">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Pix </title>
 
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 
     </head>
     <body class="">
-    <nav class="navbar">
-        <div class="logo">
-            <img src="{{asset('img/laravel.svg')}}" alt="Logo">
-            <Label>Laravel Pix </Label>
+        <nav class="navbar">
+            <div class="logo">
+                <img src="{{asset('img/laravel.svg')}}" alt="Logo">
+                <Label>Laravel Pix </Label>
+            </div>
+            <div class="user-icon" title="Usuário Logado"></div>
+        </nav>
+        <div class="container">
+                @yield('content')
         </div>
-        <div class="user-icon" title="Usuário Logado"></div>
-    </nav>
-        
-    </body>
+        </body>
 </html>
